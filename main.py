855cc06ab508f9f26faa63a2d549e563c775ec7a
jarodgrining <jarod.grining@gmail.com>
2021-09-08 17:57:12 +1000

    init

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+import sys
+
+TOKEN = sys.argv[1]
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("Logged in as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.channel.name != "general":
+        return
+
+    if "loli" in message.content.lower():
+        await message.delete()
+        await message.channel.send("Discord user {0} was caught talking about lolis in #general. For shame, discord user {0}. For shame.".format(message.author))
+
+client.run(TOKEN)
