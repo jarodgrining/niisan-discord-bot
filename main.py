b2d2c074f61f6e46f13f89312e8b372d8c3b2ac4
jarodgrining <jarod.grining@gmail.com>
2021-09-10 17:54:23 +1000

    various

--- main.py
+++ main.py
@@ -29,20 +29,21 @@ async def on_message(message):
         await send_dinner_rant(message.channel)
 
 async def has_loli(content):
-    if re.search("([lLI\|1!])([ \n]*)([oO0]+|\(\))([ \n]*)([lLI\|1!]+)([ \n]*)([iI1!\|])", content) == None:
+    if re.search("([lLI\|1!𝓵])([\.- \n]*)([oO0𝓸]+|\(\))([\.- \n]*)([lLI\|1!𝓵]+)([\.- \n]*)([iI1!\|¡𝓲])", content) == None:
         return False
     else:
         return True
 
 async def has_java(content):
-    if re.search("([jJ])([ \n]*)([aA@]+)([ \n]*)(([vV]|(\\\/))+)([ \n]*)([aA@])", content) == None:
+    if re.search("([jJ])([\.- \n]*)([aA@]+)([\.- \n]*)(([vV]|(\\\/))+)([\.- \n]*)([aA@])", content) == None:
         return False
     else:
         return True
 
 async def send_dinner_rant(channel):
-    f = open("app/dinnerrant.txt", "r")
-    await channel.send(f.read())
+    f = open("dinnerrant.txt", "r")
+    rant = f.read()
+    await channel.send(rant)
     f.close()
 
 client.run(TOKEN)
